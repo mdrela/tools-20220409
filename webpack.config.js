a426f00cde3e201b03685df3da67f4d21b1542d0
mdrela <martdrela@gmail.com>
2022-04-09 13:07:00 +0200

    SASS

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,17 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: ['./src/main.js', './src/style.css'],
+  entry: ['./src/main.js', './src/style.scss'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
   module: {
-    rules: [{
-      test: /.css$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader'],
-    },
+    rules: [
+      {
+        test: /.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader'],
+      },
     ]
   },
   plugins: [
