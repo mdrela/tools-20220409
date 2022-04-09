e779a80d05be72506cb376f7ca209d74e5fc40f1
mdrela <martdrela@gmail.com>
2022-04-09 11:56:19 +0200

    Style CSS webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: ['./src/main.js', './src/style.css'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+  },
+  module: {
+    rules: [{
+      test: /.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
   },
   plugins: [
 	new HTMLWebpackPlugin({template: './src/index.html'})
